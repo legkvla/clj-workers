ce9e97e4b6e4e859a0b33f012b1d5a465b7d9db2
Vladimir Legkunets <vladimir.legkunets@gmail.com>
2023-04-30 17:27:24 +0400

    Adding handlers

---
+++ src/clj_workers/handlers.clj
@@ -0,0 +1,158 @@
+(ns clj-workers.handlers
+  (:import
+    [java.sql Timestamp])
+  (:require
+    [reitit.ring :as ring]
+    [reitit.coercion.malli]
+    [reitit.ring.malli]
+    [reitit.swagger :as swagger]
+    [reitit.swagger-ui :as swagger-ui]
+    [reitit.ring.coercion :as coercion]
+    [reitit.dev.pretty :as pretty]
+    [reitit.ring.middleware.muuntaja :as muuntaja]
+    [reitit.ring.middleware.exception :as exception]
+    [reitit.ring.middleware.parameters :as parameters]
+    ;       [reitit.ring.middleware.dev :as dev]
+    ;       [reitit.ring.spec :as spec]
+    ;       [spec-tools.spell :as spell]
+
+    [muuntaja.core :as m]
+    [malli.util :as mu]
+
+    [clj-workers.security :refer [mw:token mw:auth check-creds make-token]]
+    [clj-workers.wrapper :refer [wrap-handler wrap-task]]))
+
+(defn handler [handler-id fn:body]
+  (fn [ctx] (wrap-handler handler-id ctx fn:body)))
+
+(def handler:swagger
+  ["/swagger.json"
+    {:get {:no-doc true
+           :swagger
+           {
+             :info
+             {
+               :title "Gateway API"
+               :description "Gateway API"}
+             :security [{:bearer []}]
+             :securityDefinitions
+             {
+               :bearer
+               {:type "apiKey"
+                :in "header"
+                :name "Authorization"}}}
+           :tags [{:name "trading", :description "Trading"}]
+           :handler (swagger/create-swagger-handler)}}])
+
+(def handler:login-token
+  ["/login-token"
+   {
+     :post
+     {
+       :summary "Login using static token"
+       :responses
+       {
+         200 {:body [:map [:jwt-token string?]]}
+         401 {:body [:map [:error string?]]}}
+       :parameters
+       {
+         :body
+         [:map
+          [:token string?]]}
+      :handler
+      (handler
+        :login-token
+        (fn [{{{:keys [token]} :body} :parameters}]
+          (if (check-creds token)
+            {
+              :status 200
+              :body {:jwt-token (make-token :admin :admin)}}
+            {
+              :status 401
+              :body {:error "Unauthorized"}})))}}])
+
+(def handler:me
+  ["/me"
+   {
+     :middleware [mw:token mw:auth]
+     :get
+     {
+       :summary "Return identity"
+       :responses
+       {
+         200
+         {:body
+           [:map
+            [:identity
+              [:map
+                [:id string?]
+                [:role string?]
+                [:iat int?]
+                [:exp int?]]]]}
+         401 {:body [:map [:error string?]]}}
+       :handler
+       (handler
+         :get-identity
+         (fn [{:keys [identity]}]
+           {
+             :status 200
+             :body {:identity identity}}))}}])
+
+(def handlers:auth
+  ["/auth"
+   {:swagger {:tags ["auth"]}}
+   handler:login-token
+   handler:me])
+
+
+(def reitit-config
+  {
+   ;;:reitit.middleware/transform dev/print-request-diffs ;; pretty diffs
+   ;;:validate spec/validate ;; enable spec validation for route data
+   ;;:reitit.spec/wrap spell/closed ;; strict top-level validation
+   :exception pretty/exception
+   :data {:coercion (reitit.coercion.malli/create
+                      {;; set of keys to include in error messages
+                       :error-keys #{#_:type :coercion :in :schema :value :errors :humanized #_:transformed}
+                       ;; schema identity function (default: close all map schemas)
+                       :compile mu/closed-schema
+                       ;; strip-extra-keys (effects only predefined transformers)
+                       :strip-extra-keys true
+                       ;; add/set default values
+                       :default-values true
+                       ;; malli options
+                       :options nil})
+          :muuntaja m/instance
+          :middleware [;; swagger feature
+                       swagger/swagger-feature
+                       ;; query-params & form-params
+                       parameters/parameters-middleware
+                       ;; content-negotiation
+                       muuntaja/format-negotiate-middleware
+                       ;; encoding response body
+                       muuntaja/format-response-middleware
+                       ;; exception handling
+                       exception/exception-middleware
+                       ;; decoding request body
+                       muuntaja/format-request-middleware
+                       ;; coercing response bodys
+                       coercion/coerce-response-middleware
+                       ;; coercing request parameters
+                       coercion/coerce-request-middleware]}})
+
+(def router
+  (ring/router
+    [handler:swagger handlers:auth]
+    reitit-config))
+
+
+(def default-routes
+  (ring/routes
+    (swagger-ui/create-swagger-ui-handler
+      {:path "/"
+       :config {:validatorUrl nil
+                :operationsSorter "alpha"}})
+    (ring/create-default-handler)))
+
+(def app-handlers
+  (ring/ring-handler router default-routes))
---
+++ src/clj_workers/security.clj
@@ -0,0 +1,61 @@
+(ns clj-workers.security
+  (:require
+    [buddy.auth :as buddy-auth]
+    [buddy.auth.backends :as backends]
+    [buddy.auth.middleware :refer [wrap-authentication]]
+    [buddy.sign.jwt :as jwt]
+    [buddy.core.keys :as keys]
+    [clj-time.core :as t]
+    ; [clojure.set :as set]
+    [clojure.tools.logging :refer [info debug]]
+
+    [environ.core :refer [env]]))
+
+(defn pubkey []
+  (if-let [public-key (env :public-key)] (keys/str->public-key public-key)))
+(defn privkey []
+  (if-let [private-key (env :private-key)] (keys/str->private-key private-key)))
+
+(def token-expiration (t/days 365))
+
+(def jwe-options {:alg :rsa-oaep
+                  :enc :a128cbc-hs256})
+
+(defn make-backend []
+  (backends/jwe {:secret (privkey)
+                 :options jwe-options}))
+
+(defn make-token [user-id role]
+  (let [now (t/now)]
+    (jwt/encrypt
+      {:id user-id :role role :iat now :exp (t/plus now token-expiration)}
+      (pubkey) jwe-options)))
+
+(defonce token-backend (make-backend))
+
+(defn mw:token
+  "Middleware used on routes requiring token authentication."
+  [handler]
+  (wrap-authentication handler token-backend))
+
+(defn mw:auth
+  "Middleware used in routes that require authentication. If request is
+  not authenticated a 401 unauthorized response will be
+  returned. Buddy checks if request key :identity is set to truthy
+  value by any previous middleware."
+  [handler]
+  (fn [request]
+    (if (buddy-auth/authenticated? request)
+      (handler request)
+      {:status 401 :body {:error "Unauthorized"}})))
+
+(defn admin-middleware
+  "Middleware used on routes requiring :admin role."
+  [handler]
+  (fn [request]
+    (if (-> request :identity :roles set (contains? "admin"))
+      (handler request)
+      {:status 403 :body {:error "Admin role required"}})))
+
+(defn check-creds [token]
+  (-> :admin-token env (= token)))
