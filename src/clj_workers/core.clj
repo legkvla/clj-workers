04a759129cead6ae2a63d4cf3da9b5a6e35e1797
Vladimir Legkunets <vladimir.legkunets@gmail.com>
2023-05-01 08:45:15 +0400

    Implementing server start

--- src/clj_workers/core.clj
+++ src/clj_workers/core.clj
@@ -1,7 +1,31 @@
 (ns clj-workers.core
-  (:gen-class))
+  (:gen-class)
+
+  (:require
+    [clj-workers.handlers :refer [app-handlers]]
+    ; [clj-workers.monitors :refer [start-monitors]]
+    [org.httpkit.server :as server]
+
+    [metrics.core :refer [default-registry]]
+    [metrics.jvm.core :refer [instrument-jvm]]
+    [metrics.reporters.console :as console]
+    [metrics.ring.expose :refer [expose-metrics-as-json]]
+    [metrics.ring.instrument :refer [instrument]]))
+
+(defn start []
+  (server/run-server
+    (-> app-handlers
+       expose-metrics-as-json)
+    {:port 3002})
+  (println "server running in port 3002"))
+
+  ;TODO We need to pause there to allow stale server die
+  ; (Thread/sleep (* 3 60 1000))
+  ; (start-workers)
+  ; (start-monitors))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Running server"
   [& args]
-  (println "Hello, World!"))
+  (instrument-jvm default-registry)
+  (start))
