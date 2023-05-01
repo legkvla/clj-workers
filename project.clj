510227751088ef487f3f516159971bd302646715
Vladimir Legkunets <vladimir.legkunets@gmail.com>
2023-05-01 08:51:06 +0400

    Updating project settings

--- project.clj
+++ project.clj
@@ -3,6 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :plugins
+  [
+    [lein-environ "1.1.0"]]
   :dependencies
   [
     [org.clojure/clojure "1.10.3"]
@@ -27,6 +30,14 @@
     [org.slf4j/slf4j-log4j12 "1.7.30"]]
 
   :main ^:skip-aot clj-workers.core
+  :min-lein-version "2.0.0"
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+  :profiles
+  {
+    :uberjar
+    {
+      :aot [clj-workers.core]
+      :main clj-workers.core
+      ; :jvm-opts ["-Dclojure.compiler.direct-linking=true"]
+      :uberjar-name "workers.jar"
+      :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
