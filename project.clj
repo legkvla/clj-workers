2fcbde8976126040a036415ca0cf9d79ef82cc18
Vladimir Legkunets <vladimir.legkunets@gmail.com>
2023-05-25 19:48:01 +0000

    Adding deps.edn for watson

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :plugins
-  [
-    [lein-environ "1.1.0"]]
+  [[lein-environ "1.1.0"]
+   [io.github.borkdude/lein-lein2deps "0.1.0"]]
   :dependencies
   [
     [org.clojure/clojure "1.10.3"]
